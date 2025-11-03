0ccd69747d58eb1243318ec33ada31c57969a9c4
justian <justiandev@gmail.com>
2025-11-03 11:16:03 +0100

    feat: show serialized geometry field in Area admin

--- app/signals/apps/signals/admin/area.py
+++ app/signals/apps/signals/admin/area.py
@@ -1,20 +1,22 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2022 - 2023 Gemeente Amsterdam
+from django.contrib.gis import forms
 from django.contrib.gis.admin import GISModelAdmin
 from import_export.admin import ExportActionMixin, ImportExportModelAdmin
 
 from signals.apps.signals.resources import AreaResource, AreaTypeResource
 
 
+class CustomOSMWidget(forms.OSMWidget):
+    template_name = 'gis/custom-openlayers-osm.html'
+
 class AreaAdmin(ImportExportModelAdmin, ExportActionMixin, GISModelAdmin):
     resource_class = AreaResource
+    gis_widget = CustomOSMWidget
 
     search_fields = ['name', 'code', '_type__name', '_type__code']
     list_display = ['name', 'code', '_type']
     list_filter = ['_type__code']
-
-    class Media:
-        js = ('admin/js/show_geometry_field.js',)
 
 
 class AreaTypeAdmin(ImportExportModelAdmin, ExportActionMixin):
--- app/signals/apps/signals/static/admin/js/show_geometry_field.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const geomField = document.getElementById('id_geometry');
-
-    if (geomField) {
-        geomField.removeAttribute('hidden');
-        geomField.style.display = 'block';
-        geomField.style.width = '100%';
-    }
-});
