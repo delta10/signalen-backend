54ef22a992b581f3142a29519ce6ebc34b418915
Bart Jeukendrup <bart@delta10.nl>
2026-01-13 15:27:46 +0100

    fix: add object storage support for dwh export and classification

--- app/signals/apps/api/views/csv.py
+++ app/signals/apps/api/views/csv.py
@@ -1,17 +1,14 @@
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (C) 2020 - 2021 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
-from glob import glob
-from os import path
-
-from django.conf import settings
 from django.http import FileResponse
-from django.utils import timezone
 from rest_framework import status
 from rest_framework.exceptions import NotFound
 from rest_framework.renderers import BaseRenderer
 from rest_framework.viewsets import ViewSet
 
 from signals.apps.api.generics.permissions import SIAPermissions, SIAReportPermissions
+from signals.apps.reporting.csv.utils import DWH_ZIP_FILENAME
+from signals.apps.reporting.utils import _get_storage_backend
 from signals.auth.backend import JWTAuthBackend
 
 
@@ -36,27 +33,16 @@ class PrivateCsvViewSet(ViewSet):
     permission_classes = (SIAPermissions & SIAReportPermissions, )
 
     def list(self, detail=True, renderer_classes=(PassthroughRenderer,)):
-        if not settings.DWH_MEDIA_ROOT:
-            raise NotFound(detail='Unconfigured Csv location', code=status.HTTP_404_NOT_FOUND)
+        storage = _get_storage_backend(using='datawarehouse')
 
-        now = timezone.now()
-        src_folder = f'{settings.DWH_MEDIA_ROOT}/{now:%Y}/{now:%m}/{now:%d}'
+        if not storage.exists(DWH_ZIP_FILENAME):
+            raise NotFound(detail='Latest CSV file not found', code=status.HTTP_404_NOT_FOUND)
 
-        if not path.exists(src_folder):
-            raise NotFound(detail='Incorrect Csv folder', code=status.HTTP_404_NOT_FOUND)
-
-        list_of_files = glob(f'{src_folder}/*.zip', recursive=True)
-        latest_file = None if not list_of_files else max(list_of_files, key=path.getctime)
-
-        if not latest_file:
-            raise NotFound(detail='No Csv files in folder', code=status.HTTP_404_NOT_FOUND)
+        # Open file from storage backend
+        file_obj = storage.open(DWH_ZIP_FILENAME, 'rb')
 
         return FileResponse(
-            open(latest_file, 'rb'),
+            file_obj,
             as_attachment=True,
-            filename=self._path_leaf(latest_file)
+            filename=DWH_ZIP_FILENAME
         )
-
-    def _path_leaf(self, file_name):
-        head, tail = path.split(file_name)
-        return tail or path.basename(head)
